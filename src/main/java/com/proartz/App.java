dd552c2f7c7b5b5cd81b53e7579092bd276c11e4
Marcin Filipowski <proartz.devon@gmail.com>
2019-03-19 13:47:09 +0100

    Add View and Model Class

--- src/main/java/com/proartz/App.java
+++ src/main/java/com/proartz/App.java
@@ -1,59 +1,17 @@
 package com.proartz;
 
 import javax.swing.*;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.util.LinkedList;
-import java.util.List;
 
-public class App extends JPanel
-                 implements ItemListener {
-
-    protected List<JToggleButton> buttons;
-    int numbersOfButtons = 50;
-    int selectionCounter;
-
-    public App() {
-        buttons = new LinkedList<JToggleButton>();
-
-        for(int i = 0; i < numbersOfButtons; i++) {
-            JToggleButton button = new JToggleButton();
-            button.setIcon(new ImageIcon("images/ufo-flying.png"));
-            button.setSelectedIcon(new ImageIcon("images/ufo.png"));
-            buttons.add(button);
-        }
-
-        for(JToggleButton button : buttons) {
-            button.addItemListener(this);
-            add(button);
-        }
-
-        selectionCounter = 0;
-    }
-
-    @Override
-    public void itemStateChanged(ItemEvent e) {
-        Object source = e.getItemSelectable();
-        if (e.getStateChange() == ItemEvent.SELECTED) {
-            if (selectionCounter < 2) {
-                selectionCounter++;
-            } else {
-                for(JToggleButton button : buttons) {
-                    button.setSelected(false);
-                }
-
-                selectionCounter = 0;
-            }
-        }
-    }
+public class App {
+    private static View view;
 
     public static void createAndShowGUI() {
         JFrame frame = new JFrame("Testing Buttons");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        App newContentPane = new App();
-        newContentPane.setOpaque(true);
-        frame.setContentPane(newContentPane);
+        view = new View();
+        view.setOpaque(true);
+        frame.setContentPane(view);
 
         frame.pack();
         frame.setVisible(true);
