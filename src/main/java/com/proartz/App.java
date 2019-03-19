cc671e23b5e783d9c31a2572cb819f10e74fd1a6
Marcin Filipowski <proartz.devon@gmail.com>
2019-03-19 13:15:52 +0100

    Add buttons

--- src/main/java/com/proartz/App.java
+++ src/main/java/com/proartz/App.java
@@ -1,69 +1,68 @@
 package com.proartz;
 
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import javax.swing.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.LinkedList;
+import java.util.List;
 
-public class App extends Frame{
+public class App extends JPanel
+                 implements ItemListener {
 
-    private static Frame frame;
-    private static int tilesInRow = 8;
-    private static int numbersOfRows = tilesInRow;
-    private static int height;
-    private static int width;
+    protected List<JToggleButton> buttons;
+    int numbersOfButtons = 50;
+    int selectionCounter;
 
-    public App(){
-        super();
+    public App() {
+        buttons = new LinkedList<JToggleButton>();
+
+        for(int i = 0; i < numbersOfButtons; i++) {
+            JToggleButton button = new JToggleButton();
+            button.setIcon(new ImageIcon("images/ufo-flying.png"));
+            button.setSelectedIcon(new ImageIcon("images/ufo.png"));
+            buttons.add(button);
+        }
+
+        for(JToggleButton button : buttons) {
+            button.addItemListener(this);
+            add(button);
+        }
+
+        selectionCounter = 0;
     }
 
-//    @Override
-//    public void paint(Graphics g) {
-//        super.paint(g);
-//        Point location = frame.getLocation();
-//        height = frame.getHeight() - (insets().top + insets().bottom);
-//        width = frame.getWidth() - (insets().left + insets().right);
-//        System.out.format("Location of the frame: %s%n", location);
-//        System.out.format("Height of the frame: %d%n", height);
-//        System.out.format("Width of the frame: %d%n", width);
-//
-//        g.setColor(Color.RED);
-//        int x = 0;
-//        int y = insets().top;
-//        int rectWidth = (width / tilesInRow) - 1;
-//        int rectHeight = (height / numbersOfRows) - 1;
-//        for(int i = 0; i < numbersOfRows; i++) {
-//            x = 0;
-//            for(int j = 0; j < tilesInRow; j++) {
-//                g.drawRect(x, y, rectWidth, rectHeight);
-//                x += rectWidth + 1;
-//            }
-//            y += rectHeight + 1;
-//        }
-//    }
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        Object source = e.getItemSelectable();
+        if (e.getStateChange() == ItemEvent.SELECTED) {
+            if (selectionCounter < 2) {
+                selectionCounter++;
+            } else {
+                for(JToggleButton button : buttons) {
+                    button.setSelected(false);
+                }
 
-    public void paint(Graphics g) {
-        // Dynamically calculate size information
-        // Dimension
-        size = getSize();
-        // diameter
-        int d = Math.min(size.width, size.height);
-        int x = (size.width - d)/2;
-        int y = (size.height - d)/2;
-        // draw circle (color already set to foreground)
-        g.fillOval(x, y, d, d);
-        g.setColor(Color.black);
-        g.drawOval(x, y, d, d);
+                selectionCounter = 0;
+            }
+        }
+    }
+
+    public static void createAndShowGUI() {
+        JFrame frame = new JFrame("Testing Buttons");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        App newContentPane = new App();
+        newContentPane.setOpaque(true);
+        frame.setContentPane(newContentPane);
+
+        frame.pack();
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-        frame = new App();
-        frame.setSize(400, 400);
-        frame.setVisible(true);
-
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
             }
         });
     }
