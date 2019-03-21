81c9f72fdef13a37fe1178dfc47d2065a50d71d0
Marcin Filipowski <proartz.devon@gmail.com>
2019-03-21 15:58:22 +0100

    Add Grid Layout

--- src/main/java/com/proartz/Controller.java
+++ src/main/java/com/proartz/Controller.java
@@ -1,6 +1,7 @@
 package com.proartz;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.io.File;
@@ -30,8 +31,9 @@ public class Controller {
     public void createButtons(int numberOfButtons) {
         //check is numberOfButtons a square number
         //so we can have square board(rows == columns)
-        if(Math.sqrt(numberOfButtons) % 1 != 0) {
-            System.err.println("Error: numberOfRows need to be a square of an integer :(");
+        if(((int)Math.sqrt(numberOfButtons) % 1) != 0) {
+            System.err.println("Error: numberOfButtons need to be a square of an integer :(");
+            System.exit(-1);
         }
 
         int numberOfRows = (int)Math.sqrt(numberOfButtons);
@@ -49,25 +51,54 @@ public class Controller {
         List<JToggleButton> buttons = view.getButtons();
         JPanel contentPane = view.getContentPane();
 
+        contentPane.setLayout(new GridLayout(numberOfRows, numberOfColumns));
+
+        int iconCounter = 0;
+
         for(int i = 0; i < numberOfRows; i++) {
             for(int j = 0; j < numberOfColumns; j++) {
 
                 JToggleButton button = new JToggleButton();
                 button.setIcon(new ImageIcon("images/037-ufo-flying.png"));
-                button.setSelectedIcon(new ImageIcon(icons.get(i)));
+                button.setSelectedIcon(new ImageIcon(icons.get(iconCounter)));
+                iconCounter++;
 
                 button.addItemListener(new ItemListener() {
                     @Override
                     public void itemStateChanged(ItemEvent e) {
                         Object source = e.getItemSelectable();
                         if (e.getStateChange() == ItemEvent.SELECTED) {
-                            if (model.getSelectionCounter() < 2) {
+                            if (model.getSelectionCounter() == 0) {
                                 model.incrementCounter();
-                            } else {
-                                for (JToggleButton button : view.getButtons()) {
-                                    button.setSelected(false);
+                                model.getSelectedTile1().setReference(e);
+                            }
+                            else if(model.getSelectionCounter() == 1) {
+                                model.incrementCounter();
+                                model.getSelectedTile2().setReference(e);
+
+                                if(compareTiles()) {
+                                    model.decrementToGuess();
+
+                                    if(model.getToGuess() == 0) {
+                                        System.out.println("Gratuluję!!! Wygrałeś!!!");
+                                    }
+                                } else {
+                                    resetButtonSelection();
                                 }
                                 model.resetCounter();
+                                model.setSelectedTile1(null);
+                                model.setSelectedTile2(null);
+
+                            }
+                            if (e.getStateChange() == ItemEvent.SELECTED) {
+                                if (model.getSelectionCounter() < 2) {
+                                    model.incrementCounter();
+                                } else {
+                                    for (JToggleButton button : view.getButtons()) {
+                                        button.setSelected(false);
+                                    }
+                                    model.resetCounter();
+                                }
                             }
                         }
                     }
@@ -88,7 +119,7 @@ public class Controller {
         File[] fileList = fileName.listFiles();
 
         for (File file: fileList) {
-            System.out.println(file);
+//            System.out.println(file);
 
             //add icon twice
             icons.add(file.toString());
@@ -97,5 +128,20 @@ public class Controller {
         Collections.shuffle(icons);
 
         return icons;
+    }
+
+    public boolean compareTiles() {
+        JToggleButton button1 = (JToggleButton)model.getSelectedTile1().getReference();
+        JToggleButton button2 = (JToggleButton)model.getSelectedTile2().getReference();
+
+        return  button1.getSelectedIcon().equals(button2.getSelectedIcon());
+    }
+
+    public void resetButtonSelection() {
+        JToggleButton button1 = (JToggleButton)model.getSelectedTile1().getReference();
+        JToggleButton button2 = (JToggleButton)model.getSelectedTile2().getReference();
+
+        button1.setSelected(false);
+        button2.setSelected(false);
     }
 }
--- src/main/java/com/proartz/Model.java
+++ src/main/java/com/proartz/Model.java
@@ -3,12 +3,18 @@ package com.proartz;
 public class Model<T> {
     private int selectionCounter;
     private int numberOfTiles;
+    private int toGuess;
     private Board board;
+
+    private Tile selectedTile1, selectedTile2;
 
     public Model() {
         selectionCounter = 0;
         numberOfTiles = 50;
+        toGuess = numberOfTiles / 2;
         board = new Board(numberOfTiles);
+        selectedTile1 = new Tile(null);
+        selectedTile2 = new Tile(null);
     }
 
     public Board getBoard() {
@@ -29,5 +35,33 @@ public class Model<T> {
 
     public int getNumberOfTiles() {
         return numberOfTiles;
+    }
+
+    public Tile getSelectedTile1() {
+        return selectedTile1;
+    }
+
+    public void setSelectedTile1(Tile selectedTile1) {
+        this.selectedTile1 = selectedTile1;
+    }
+
+    public Tile getSelectedTile2() {
+        return selectedTile2;
+    }
+
+    public void setSelectedTile2(Tile selectedTile2) {
+        this.selectedTile2 = selectedTile2;
+    }
+
+    public void decrementToGuess() {
+        toGuess--;
+    }
+
+    public int getToGuess() {
+        return toGuess;
+    }
+
+    public void setToGuess(int toGuess) {
+        this.toGuess = toGuess;
     }
 }
